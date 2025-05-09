bcd908d61a5ab0cd69865f6f49ddce0c66fb5fdf
Pruthviraj Rathod <prithvirathod29884@gmail.com>
2025-05-09 21:21:30 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='MedicalChatbot',
+    version='0.0.1',
+    author='Pruthviraj Rathod',
+    author_email='prithvirathod29884@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
